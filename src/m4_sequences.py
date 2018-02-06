1c83ab2edc41be6c0844a785d8fb6dbeecdf4df2
tomlingb <tomlingb@rose-hulman.edu>
2018-02-05 23:44:06 -0500

    finished this one

--- src/m4_sequences.py
+++ src/m4_sequences.py
@@ -5,8 +5,8 @@ This problem provides practice at:
   ***  SEQUENCES.  ***
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Geoffrey Tomlinson.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ########################################################################
 # Students:
@@ -133,8 +133,15 @@ def practice_problem4a(sequence):
     Type hints:
       :type sequence: list | tuple | string
     """
+
+    indices = []
+    for k in range(len(sequence) - 1):
+        if sequence[k + 1] == sequence[k]:
+            indices.append(k)
+    return indices
+
     ####################################################################
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #     The testing code is already written for you (above).
     ####################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
@@ -196,8 +203,19 @@ def practice_problem4b(sequence):
     Type hints:
       :type sequence: (list | tuple) of (float | int)
     """
+
+    count = 0
+    for k in range(0, len(sequence), 2):
+        if count == 0:
+            number = sequence[k]
+            count += 1
+        if sequence[k] > number:
+            number = sequence[k]
+    return number
+
+
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #     The testing code is already written for you (above).
     ####################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
@@ -294,8 +312,19 @@ def practice_problem4c(points):
       :type points: tuple of rg.Point
       :rtype: rg.Point | string
     """
+
+    for k in range(len(points)):
+        point = points[k]
+        if is_prime(point.x) and is_prime(point.y):
+            x = point.x
+            y = point.y
+            point.x = y
+            point.y = x
+            return point
+    return 'Not found'
+
     ####################################################################
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     # IMPORTANT: This problem is your LOWEST PRIORITY for preparing
@@ -390,6 +419,15 @@ def practice_problem4d(sequence):
       :type sequence: (list | tuple) of int
       :rtype: int
     """
+
+    number = 0
+    for k in range(len(sequence) - 1):
+        if is_prime(sequence[k]) and sequence[k] != sequence[k + 1] and \
+                is_prime(sequence[k + 1]):
+            number += sequence[k]
+    return number
+
+
     ####################################################################
     # TODO: 5. Implement and test this function.
     #     The testing code is already written for you (above).
